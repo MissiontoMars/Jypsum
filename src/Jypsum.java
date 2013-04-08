d34ac49d2f9ca118ef43edf772d85b958da8b5bf
mute <justinreves.tch@gmail.com>
2013-04-08 15:06:52 -0400

    Just kicking the tires on the repo

---
+++ src/Jypsum.java
@@ -0,0 +1,11 @@
+package Jypsum;
+public class Jypsum {
+    public Jypsum () {
+    }
+    public static void main (String[] args) {
+        for(int i = 0; i < args.length; i++) {
+            //Do options
+        }
+        
+
+    }
