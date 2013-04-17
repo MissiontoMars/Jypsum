0c0e260a41baf7b3469ee1d1dd67064f1935591f
wintermute <justinreeves.tch@gmail.com>
2013-04-16 20:08:26 -0400

    SMTPClient.java filled out; need header/body

--- src/net.mua.jypsum/networking/SMTPClient.java
+++ src/net.mua.jypsum/networking/SMTPClient.java
@@ -3,10 +3,10 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-import net.mua.jypsum.mail.Envelope;
+import net.mua.jypsum.mail.networking;
 
 public class SMTPClient {
-    private Socket connection;
+    private Socket clientSocket;
     private BufferedReader fromServer;
     private DataOutputStream toServer;
     private static final int SMTP_PORT = 25;
@@ -17,33 +17,55 @@ public class SMTPClient {
      */
     public SMTPClient(Envelope env) throws IOException {
         connection = new Socket(env.destHost, SMTP_PORT);
-        fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         toServer = new DataOutputStream(clientSocket.getOutputStream());
-        private String localhost = ;
-        sendCommand(); //Handshake
+        private String localhost = InetAddress.getLocalHost().toString();
+        sendCommand("HELO", 354); //Handshake
         isConnected = true;
     }
     
     public void send(Envelope myEnv) throws IOException {
-        //SMTP commands in order
+        sendCommand("MAIL FROM", myEnv.recipient, 250);
+        sendCommand("RCPT TO", 251 || 250);
+        sendCommand("DATA", 354);
+        sendCommand("");
+        this.close();
     }
     
     public void close() {
         isConnected = false;
         try {
-            sendCommand(); //close
+            sendCommand("QUIT", 221); //close
         } catch (IOException e) {
             System.err.println("Unable to close connection: " + e);
             isConnected = true;
         }
     }
     
-    private void sendCommand(String cmd, int expReply) throws IOException {
+    private void sendCommand(String cmd, String append, String int expReply) throws IOException {
         //command to server
+        if (!append.isEmpty) {
+            toServer.writeBytes(cmd + " " +  append + CRLF);
+        } else {
+            toServer.writeBytes(cmd + CRLF);
+        }
         //read reply
+        int replyCode = parseReply(fromServer.readLine());
         //verify server reply code is same as parameter
         //throw exception if not
+        if (replyCode == expCode) {
+        } else {
+            System.out.println("Rcv code " + replyCode);
+            System.out.println("expected " + expCode);
+            throw (IOException e);
+        }
         
+    }
+    
+    //Sugar for sending commands without user input fields
+    //Like HELO
+    private void sendCommand(String cmd, String expReply) throws IOException {
+        sendCommand(cmd, "", expReply);
     }
     
     private int parseReply(String reply) {
